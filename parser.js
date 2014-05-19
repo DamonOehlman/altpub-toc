bd9a66d45e4974457a45ba0d850a1318333c94d8
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-19 15:58:23 +1000

    First working implementation of the parser

--- parser.js
+++ parser.js
@@ -1,3 +1,40 @@
+// define a few regexes
+var reCommentLine = /^\#/;
+var reLineBreak = /\n\r?/;
+var reSection = /\:$/;
+
 module.exports = function(content) {
-  return [];
+  var section;
+  var lines = content.split(reLineBreak).map(function(line) {
+    return line.trim();
+  }).filter(Boolean);
+
+  // initialise the toc
+  var toc = section = [];
+
+  function activateSection(sectionName) {
+    if (sectionName === 'mainmatter') {
+      return section = toc;
+    }
+
+    // ensure the section array exists
+    toc[sectionName] = toc[sectionName] || [];
+    return section = toc[sectionName];
+  }
+
+  // iterate through the lines and create the content
+  lines.forEach(function(line) {
+    // if we have a comment line do nothing
+    if (reCommentLine.test(line)) {
+      return;
+    }
+    else if (reSection.test(line)) {
+      return activateSection(line.slice(0, -1));
+    }
+    else {
+      section.push(line);
+    }
+  });
+
+  return toc;
 };
