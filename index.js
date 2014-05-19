193580890d14c114f1034687412a79320c25665e
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-19 15:46:50 +1000

    gendocs

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+/**
+  # altpub-toc
+
+  A leanpub compatible table of contents (Book.txt) file reader. This
+  module provides the ability to parse a leanpub (or similar) TOC file for
+  some alternative local rendering.
+
+  ## Example Usage
+
+  <<< examples/simple.js
+
+**/
 exports.load = require('./load');
